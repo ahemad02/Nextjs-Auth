f4cbd57f532c969aa5193f165824ecbb0a81ff1e
Ahemad <ahemadbakali26@gmail.com>
2025-02-27 12:02:47 +0530

    "mailer const"

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -18,7 +18,7 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       });
     }
 
-    let transport = nodemailer.createTransport({
+    const transport = nodemailer.createTransport({
       host: "sandbox.smtp.mailtrap.io",
       port: 2525,
       auth: {
